5814524eb3e452723e2c7ce2f0aab15adc48b8fd
Megan Boyer <94564195+MeganMB197674@users.noreply.github.com>
2023-05-12 21:05:15 -0400

    Add files via upload

---
+++ Code.js
@@ -0,0 +1,6 @@
+/* Megan Boyer 
+CSC256 Assignment One 
+5/12/2023 */
+
+// Print my information to the console.
+console.log('Megan M. Boyer \n Advancing Computer Science Major \n Expected Graduation: August 2024 \n Contact: megboyer@uat.edu'); 
